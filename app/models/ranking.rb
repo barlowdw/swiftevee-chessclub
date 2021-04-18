587d5f663da9ab30da4870319134e0c50ef16300
Derrick <barlow.dw@gmail.com>
2021-04-18 17:13:13 +0200

    Added move method to ranking.

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -1,9 +1,24 @@
 class Ranking < ApplicationRecord
-    validates_presence_of :member, :position
+    validates_presence_of :member
     belongs_to :member
-    before_validation :assign_position
     before_create :assign_position
-    attr_readonly :position
+
+    def move(n)
+        move_up = n < 0
+
+        return if n == 0 || (move_up && self.position == 1) || (!move_up && self.position == max_position)
+
+        ActiveRecord::Base.transaction do
+            position_difference = move_up ? -1 : +1
+            new_position = self.position + position_difference
+
+            Ranking.find_by_position!(new_position).update!(position: self.position)
+            self.update!(position: new_position)
+
+            # Call recursive with n moving closer to 0 by 1 each call
+            self.move(n - position_difference)
+        end
+    end
 
     private
 
--- spec/models/ranking_spec.rb
+++ spec/models/ranking_spec.rb
@@ -21,4 +21,51 @@ RSpec.describe Ranking, type: :model do
       expect(ranking3.position).to eq(3)
     end
   end
+
+  describe "#move" do
+    before(:each) do
+      @rankings = [
+        Member.create!(first_name: "Derrick", last_name: "Barlow", email: "barlow.dw@gmail.com", date_of_birth: Date.new(1990, 1, 10)).ranking,
+        Member.create!(first_name: "Bill", last_name: "Bailey", email: "bailey.mr@gmail.com", date_of_birth: Date.new(1965, 1, 13)).ranking,
+        Member.create!(first_name: "Kimmy", last_name: "Carr", email: "carr.j@gmail.com", date_of_birth: Date.new(1972, 9, 15)).ranking,
+        Member.create!(first_name: "Ricky", last_name: "Gervais", email: "gervais.rd@gmail.com", date_of_birth: Date.new(1961, 6, 25)).ranking
+      ]
+    end
+
+    it "should move up 1" do
+      @rankings[2].move(-1)
+
+      expect(@rankings.map { |ranking| ranking.reload.position }).to eq([1, 3, 2, 4])
+    end
+
+    it "should move down 1" do
+      @rankings[2].move(1)
+
+      expect(@rankings.map { |ranking| ranking.reload.position }).to eq([1, 2, 4, 3])
+    end
+
+    it "should not move up 1 if at top" do
+      @rankings[0].move(-1)
+
+      expect(@rankings.map { |ranking| ranking.reload.position }).to eq([1, 2, 3, 4])
+    end
+
+    it "should not move down 1 if at bottom" do
+      @rankings[3].move(1)
+
+      expect(@rankings.map { |ranking| ranking.reload.position }).to eq([1, 2, 3, 4])
+    end
+
+    it "should not move past top" do
+      @rankings[2].move(-5)
+
+      expect(@rankings.map { |ranking| ranking.reload.position }).to eq([2, 3, 1, 4])
+    end
+
+    it "should not move past bottom" do
+      @rankings[1].move(5)
+
+      expect(@rankings.map { |ranking| ranking.reload.position }).to eq([1, 4, 2, 3])
+    end
+  end
 end
