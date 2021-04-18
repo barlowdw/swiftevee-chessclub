ede5b8a7bb3980161fd6b73c157c29d52dece520
Derrick <barlow.dw@gmail.com>
2021-04-18 12:16:29 +0200

    Added models tests.

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Member, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#valid?" do
+    it "should be valid when empty" do
+      expect(Member.new).to be_valid
+    end
+
+    it "should be valid with basic attributed" do
+      expect(Member.new(first_name: "Derrick", last_name: "Barlow", email: "barlow.dw@gmail.com", date_of_birth: Date.new(1990, 1, 10))).to be_valid
+    end
+  end
 end
