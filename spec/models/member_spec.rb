a8b1b130f616ed4f0078f0c19af12a54fb6ac0b1
Derrick <barlow.dw@gmail.com>
2021-04-18 12:32:25 +0200

    Test: Unique email and additional email validation. (WIP)

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -24,6 +24,20 @@ RSpec.describe Member, type: :model do
 
     it "should be valid with basic attributed" do
       expect(Member.new(first_name: "Derrick", last_name: "Barlow", email: "barlow.dw@gmail.com", date_of_birth: Date.new(1990, 1, 10))).to be_valid
+      expect(Member.new(first_name: "Bill", last_name: "Bailey", email: "barlow.dw@gmail.com", date_of_birth: Date.new(1965, 1, 13))).to be_valid
+    end
+
+    it "should not be valid with invalid email" do
+      expect(Member.new(first_name: "Derrick", last_name: "Barlow", email: "barlow.dw@gmail.com", date_of_birth: Date.new(1990, 1, 10))).not_to be_valid
+      expect(Member.new(first_name: "Derrick", last_name: "Barlow", email: "barlow.dwgmail.com", date_of_birth: Date.new(1990, 1, 10))).not_to be_valid
+      expect(Member.new(first_name: "Derrick", last_name: "Barlow", email: "barlow.dw@com", date_of_birth: Date.new(1990, 1, 10))).not_to be_valid
+      expect(Member.new(first_name: "Derrick", last_name: "Barlow", email: "@gmail.com", date_of_birth: Date.new(1990, 1, 10))).not_to be_valid
+      expect(Member.new(first_name: "Derrick", last_name: "Barlow", email: "gmail.com", date_of_birth: Date.new(1990, 1, 10))).not_to be_valid
+    end
+
+    it "should not be valid with duplicate email" do
+      Member.create!(first_name: "Derrick", last_name: "Barlow", email: "barlow.dw@gmail.com", date_of_birth: Date.new(1990, 1, 10))
+      expect(Member.new(first_name: "Bill", last_name: "Bailey", email: "barlow.dw@gmail.com", date_of_birth: Date.new(1965, 1, 13))).not_to be_valid
     end
   end
 end
